7c137e846a3736079de3cce21a613522858cb837
abusch8 <abusch8@huskers.unl.edu>
2023-03-31 15:37:09 -0500

    Initial commit, api example

---
+++ midb.py
@@ -0,0 +1,6 @@
+import requests
+import json
+
+api_url = 'https://imdb-api.com/en/API/Top250Movies/k_k1c2il8w'
+response = requests.get(api_url)
+print(json.dumps(response.json(), indent=4, separators=(". ", " = ")))
