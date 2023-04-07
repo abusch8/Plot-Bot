4b0c5ef698c4f48d7e42d32ad5dbe82d3fe5907a
abusch8 <abusch8@huskers.unl.edu>
2023-04-07 14:44:28 -0500

    Search script loop

--- search.py
+++ search.py
@@ -1,16 +1,29 @@
 import requests
 import json
 
-search_term = input('Search: ')
+def search_api(term):
+    api_url = f'https://imdb-api.com/en/API/SearchMovie/k_k1c2il8w/{term}'
+    response = requests.get(api_url)
+    result = response.json()['results']
+    return result
 
-api_url = f'https://imdb-api.com/en/API/SearchMovie/k_k1c2il8w/{search_term}'
-response = requests.get(api_url)
-result = response.json()['results']
-for x in range(len(result)):
-    print(f'{x + 1}) {result[x]["title"]}')
-index = int(input('Select a result: ')) - 1
+def get_movie_info(id):
+    api_url = f'https://imdb-api.com/en/API/Title/k_k1c2il8w/{id}'
+    response = requests.get(api_url)
+    result = json.dumps(response.json(), indent=4, separators=(', ', ' = '))
+    return result
 
-api_url = f'https://imdb-api.com/en/API/Title/k_k1c2il8w/{result[index]["id"]}'
-response = requests.get(api_url)
-result = json.dumps(response.json(), indent=4, separators=(', ', ' = '))
-print(result)
+if __name__ == '__main__':
+    selections = []
+    for x in range(3):
+        search_term = input('Search: ')
+
+        result = search_api(search_term)
+        for x in range(len(result)):
+            print(f'{x + 1}) {result[x]["title"]}')
+        index = int(input('Select a result: ')) - 1
+
+        selections.append(result[index])
+
+    for movie in selections:
+        print(f'####\n{movie["title"].upper()}\n####\n', get_movie_info(movie["id"]))
