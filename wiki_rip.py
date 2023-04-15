55cb028b7457e9c48f41eb501d8741133f13c3e5
abusch8 <abusch8@huskers.unl.edu>
2023-04-15 17:06:01 -0500

    Syntax

--- wiki_rip.py
+++ wiki_rip.py
@@ -2,6 +2,8 @@ import requests
 import json
 import re
 
+OUTPUT_PATH = 'dataset.txt'
+
 def get_film_list(category):
     api_url = f'https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:{category}&cmlimit=500&format=json'
     res = requests.get(api_url).json()
@@ -22,10 +24,10 @@ def parse_plot(page):
         if is_plot: plot.append(line)
     return plot
 
-def clear_file(path): open(path, 'w').close()
+def clear_file(): open(OUTPUT_PATH, 'w').close()
 
-def write_file(path, line):
-    file = open(path, 'a')
+def write_file(line):
+    file = open(OUTPUT_PATH, 'a')
     file.write(line)
     file.close()
 
@@ -33,8 +35,7 @@ def json_to_string(data):
     return json.dumps(data, indent=4, separators=(', ', ' = '))
 
 if __name__ == '__main__':
-    path = 'output.txt'
-    clear_file(path)
+    clear_file()
 
     film_list = get_film_list('American_science_fiction_films')
     print(json_to_string(film_list))
@@ -43,4 +44,4 @@ if __name__ == '__main__':
         film_page = get_film_page(film_list[x]['pageid'])
         plot = parse_plot(film_page)
         print('Writing file...')
-        for line in plot: write_file(path, f'{line}\n')
+        for line in plot: write_file(f'{line}\n')
