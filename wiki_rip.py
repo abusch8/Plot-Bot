2984b2bde4dd6d44274018a02088c5ec5b59fec2
abusch8 <abusch8@huskers.unl.edu>
2023-04-16 12:07:02 -0500

    Improved api and plot parsing, film title file writing

--- wiki_rip.py
+++ wiki_rip.py
@@ -12,17 +12,19 @@ def get_film_list():
     return res['query']['categorymembers']
 
 def get_film_page(id):
-    api_url = f'https://en.wikipedia.org/w/api.php?action=query&pageids={id}&prop=revisions&rvprop=content&format=json'
+    api_url = f'https://en.wikipedia.org/w/api.php?action=parse&pageid={id}&prop=wikitext&format=json'
     res = requests.get(api_url).json()
-    return res['query']['pages'][str(id)]['revisions'][0]['*']
+    return res['parse']['wikitext']['*']
 
 def parse_plot(page):
     plot = []
     lines = page.splitlines()
     is_plot = False
     for line in lines:
-        if line == '==Plot==': is_plot = True
-        elif re.match(r'^==.*==$', line): is_plot = False
+        if line == '==Plot==':
+            is_plot = True
+            continue
+        elif re.match(r'^.*==.*==.*$', line): is_plot = False
         if is_plot: plot.append(line)
     return plot
 
@@ -50,4 +52,5 @@ if __name__ == '__main__':
         plot = parse_plot(film_page)
 
         print(f'Writing "{film_title}" [ID:{film_id}] to file...')
+        write_file(f'####{film_title}####\n')
         for line in plot: write_file(f'{line}\n')
