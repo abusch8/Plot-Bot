37ca5277378881e1ea603c5f9fb7ca90d05e43ad
abusch8 <abusch8@huskers.unl.edu>
2023-04-21 02:08:14 -0500

    Genre picker

--- wiki_rip.py
+++ wiki_rip.py
@@ -1,26 +1,25 @@
-import sys
 import requests as req
 import json
 import re
 
 API_URL = 'https://en.wikipedia.org/w/api.php'
-GENRE = 'American_science_fiction_films'
-OUTPUT_PATH = f'data/{GENRE}.txt'
 
 memory = []
 
-def get_film_list(id=None):
+def get_category(id=None, genre=None):
+    if not id and not genre: SystemExit('')
     try:
         res = req.get(API_URL, {
             'action': 'query',
             'list': 'categorymembers',
-            **({'cmtitle': f'Category:{GENRE}'} if id is None else {'cmpageid': id}),
+            **({'cmtitle': f'Category:{genre}'} if genre else {}),
+            **({'cmpageid': id} if id else {}),
             'cmlimit': 500,
             'format': 'json'
         })
+        print(res.json())
         res.raise_for_status()
-    except req.exceptions.RequestException as err:
-        SystemExit(err)
+    except req.exceptions.RequestException as err: SystemExit(err)
     return res.json()['query']['categorymembers']
 
 def get_film_page(id):
@@ -32,8 +31,7 @@ def get_film_page(id):
             'format': 'json'
         })
         res.raise_for_status()
-    except req.exceptions.RequestException as err:
-        SystemExit(err)
+    except req.exceptions.RequestException as err: SystemExit(err)
     return res.json()['parse']['wikitext']['*']
 
 def preprocess(text):
@@ -73,6 +71,9 @@ def parse_plot(page):
         elif is_plot: plot += f'{preprocess(line)}\n'
     return plot.strip()
 
+def json_to_string(data):
+    return json.dumps(data, indent=4, separators=(', ', ' = '))
+
 def expand_category(category):
     print('\033[1m\033[3m####\nEXPANDING CATEGORY\n####\033[0m')
     print(json_to_string(category))
@@ -80,31 +81,43 @@ def expand_category(category):
         title = re.sub(r'\ \(.*film\)', '', page['title'])
         page_id = page['pageid']
         if page_id in memory:
-            print(f'\033[91mSKIPPED\033[0m \033[1m\33[3m{title}\033[0m \033[96m[ID:{page_id}]\033[0m Reason: Duplicate')
+            print(f'\033[91mSKIPPED\033[0m \033[1m\033[3m{title}\033[0m \033[96m[ID:{page_id}]\033[0m Reason: Duplicate')
             continue
         memory.append(page_id)
         if re.match(r'^File:.*$', title): continue
         if re.match(r'^Category:.*$', title):
-            expand_category(get_film_list(page_id))
+            expand_category(get_category(page_id))
             continue
         plot = parse_plot(get_film_page(page_id))
         if plot:
-            print(f'\033[92mWRITING\033[0m \033[1m\033[3m{title}\033[0m \033[96m[ID:{page_id}]\033[0m Output: {OUTPUT_PATH}')
+            print(f'\033[92mWRITING\033[0m \033[1m\033[3m{title}\033[0m \033[96m[ID:{page_id}]\033[0m Output: {output_path}')
             file.write(f'####{title}####\n{plot}\n\n')
         else:
             print(f'\033[91mSKIPPED\033[0m \033[1m\033[3m{title}\033[0m \033[96m[ID:{page_id}]\033[0m Reason: No plot')
 
-def json_to_string(data):
-    return json.dumps(data, indent=4, separators=(', ', ' = '))
+def get_genre_options():
+    options = []
+    genre_list = get_category(genre='American_films_by_genre')
+    for i in range(len(genre_list)):
+        options.append(genre_list[i])
+    return options
 
-def open_file():
-    open(OUTPUT_PATH, mode='w').close()
-    return open(OUTPUT_PATH, mode='a', encoding="utf-8")
+def open_file(path):
+    open(path, mode='w').close()
+    return open(path, mode='a', encoding="utf-8")
 
 if __name__ == '__main__':
-    if sys.argv: GENRE = sys.argv[1]
     global file
-    file = open_file()
-    expand_category(get_film_list())
+    global output_path
+    options = get_genre_options()
+    for i in range(len(options)):
+        title = re.sub(r'Category:', '', options[i]["title"])
+        print(f'{i + 1}) {title}')
+    choice = int(input('Genre: ')) - 1
+    title = re.sub(r'Category:', '', options[choice]["title"])
+    output_path = f'data/{title}.txt'
+    file = open_file(output_path)
+    if choice not in range(len(options)): SystemExit('')
+    expand_category(get_category(options[choice]['pageid']))
     file.close()
     print('\033[1m\33[3m####\nCOMPLETED\n####\033[0m')
