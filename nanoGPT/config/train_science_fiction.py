a50a4f7d2061df3883dd2fd797a1d372fa8adc1d
Alex Busch <69646582+abusch8@users.noreply.github.com>
2023-04-26 22:59:17 -0500

    Began training model

---
+++ nanoGPT/config/train_science_fiction.py
@@ -0,0 +1,28 @@
+out_dir = 'out-science-fiction'
+eval_interval = 250
+eval_iters = 200
+log_interval = 10
+
+always_save_checkpoint = False
+
+wandb_log = False # override via command line if you like
+wandb_project = 'science-fiction'
+wandb_run_name = 'mini-gpt'
+
+dataset = 'American science fiction films'
+gradient_accumulation_steps = 1
+batch_size = 64
+block_size = 256 # context of up to 256 previous characters
+
+n_layer = 6
+n_head = 6
+n_embd = 384
+dropout = 0.2
+
+learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+max_iters = 5000
+lr_decay_iters = 5000 # make equal to max_iters usually
+min_lr = 1e-4 # learning_rate / 10 usually
+beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+
+warmup_iters = 100
