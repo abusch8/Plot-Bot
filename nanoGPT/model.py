5791a1986f0e1fd68b84a618f5944f3befc4529b
Alex Busch <69646582+abusch8@users.noreply.github.com>
2023-04-27 23:19:30 -0500

    Infinite sampling, continued training

--- nanoGPT/model.py
+++ nanoGPT/model.py
@@ -367,3 +367,24 @@ class GPT(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1)
 
         return idx
+
+    @torch.no_grad()
+    def infinite_generate(self, idx, temperature=1.0, top_k=None):
+        # if the sequence context is growing too long we must crop it at block_size
+        idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
+        # forward the model to get the logits for the index in the sequence
+        logits, _ = self(idx_cond)
+        # pluck the logits at the final step and scale by desired temperature
+        logits = logits[:, -1, :] / temperature
+        # optionally crop the logits to only the top k options
+        if top_k is not None:
+            v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+            logits[logits < v[:, [-1]]] = -float('Inf')
+        # apply softmax to convert logits to (normalized) probabilities
+        probs = F.softmax(logits, dim=-1)
+        # sample from the distribution
+        idx_next = torch.multinomial(probs, num_samples=1)
+        # append sampled index to the running sequence and continue
+        idx = torch.cat((idx, idx_next), dim=1)
+
+        return idx
--- nanoGPT/sample.py
+++ nanoGPT/sample.py
@@ -2,6 +2,7 @@
 Sample from a trained model
 """
 import os
+import sys
 import pickle
 from contextlib import nullcontext
 import torch
@@ -12,11 +13,12 @@ from model import GPTConfig, GPT
 init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
 out_dir = 'out' # ignored if init_from is not 'resume'
 start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
-num_samples = 10 # number of samples to draw
-max_new_tokens = 5000 # number of tokens generated in each sample
-temperature = 1.2 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
-top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
-seed = 1337
+# num_samples = 1 # number of samples to draw
+# max_new_tokens = 10000 # number of tokens generated in each sample
+temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
+top_k = 1000 # retain only the top_k most likely tokens, clamp others to have 0 probability
+# seed = 1337
+seed = 1818
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'float16' # 'float32' or 'bfloat16' or 'float16'
 compile = False # use PyTorch 2.0 to compile the model to be faster
@@ -81,9 +83,19 @@ start_ids = encode(start)
 x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
 
 # run generation
+# with torch.no_grad():
+#     with ctx:
+#         for k in range(num_samples):
+#             y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
+#             print(decode(y[0].tolist()))
+#             print('---------------')
+
 with torch.no_grad():
     with ctx:
-        for k in range(num_samples):
-            y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
-            print(decode(y[0].tolist()))
-            print('---------------')
+        idx = x
+        while True:
+            # print("-----")
+            sys.stdout.flush()
+            idx = model.infinite_generate(idx, temperature=temperature, top_k=top_k)
+            # idx = model.infinite_generate(idx, temperature=temperature)
+            print(decode(idx[0].tolist()))
