709aa52e89ac700044a33283706de8ead1b442d3
Phish <nathanhongzliew@gmail.com>
2023-05-02 13:06:33 -0500

    Add changes to test logic

--- evaluation/standfordnlp.py
+++ evaluation/standfordnlp.py
@@ -1,11 +1,43 @@
+#pip install stanfordnlp
+#(https://stanfordnlp.github.io/CoreNLP/)
+
+
+#import stanfordnlp
+#from stanfordnlp.pipeline.core import CoreNLPClient
+
+## Initialize StanfordCoreNLP
+#stanfordnlp.download('en')  # Download English models if not already downloaded
+#client = CoreNLPClient()
+
+#def check_sentence_logic(sentence):
+#    # Annotate the sentence with various annotations
+#    ann = client.annotate(sentence, properties={
+#        'annotators': 'parse',
+#        'outputFormat': 'json'
+#    })
+
+#    # Extract the parse tree from the annotation
+#    parse_tree = ann['sentences'][0]['parse']
+
+#    # Perform logic/coherence checks based on the parse tree
+#    # You can implement your own logic and coherence checks here
+
+#    # Example: Check if the sentence is a well-formed question
+#    is_question = parse_tree.startswith('(ROOT (SBARQ')
+
+#    return is_question
+
+#sentence = "What is the capital of France?"
+#is_logical = check_sentence_logic(sentence)
+#print(f"Is the sentence logical? {is_logical}")
+
+
 import stanfordnlp
 from stanfordnlp.pipeline.core import CoreNLPClient
 
 # Initialize StanfordCoreNLP
 stanfordnlp.download('en')  # Download English models if not already downloaded
 client = CoreNLPClient()
-#pip install stanfordnlp
-#(https://stanfordnlp.github.io/CoreNLP/)
 
 def check_sentence_logic(sentence):
     # Annotate the sentence with various annotations
@@ -17,14 +49,22 @@ def check_sentence_logic(sentence):
     # Extract the parse tree from the annotation
     parse_tree = ann['sentences'][0]['parse']
 
-    # Perform logic/coherence checks based on the parse tree
-    # You can implement your own logic and coherence checks here
+    # Check if the sentence has a subject and a predicate
+    has_subject = '(NP' in parse_tree
+    has_predicate = '(VP' in parse_tree
+
+    # Perform additional logic checks based on the parse tree
+    # You can implement more logic checks as needed
 
     # Example: Check if the sentence is a well-formed question
     is_question = parse_tree.startswith('(ROOT (SBARQ')
 
-    return is_question
+    # Determine if the sentence is coherent based on the logic checks
+    is_coherent = has_subject and has_predicate and not is_question
 
-sentence = "What is the capital of France?"
+    return is_coherent
+
+# Test the function with a sample sentence
+sentence = "The cat jumps."
 is_logical = check_sentence_logic(sentence)
 print(f"Is the sentence logical? {is_logical}")
