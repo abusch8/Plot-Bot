8d1d4ecba1ce8ba284d33214cff5d38ca50f6c16
Alex Busch <69646582+abusch8@users.noreply.github.com>
2023-05-02 19:41:28 -0500

    Began preparing new dataset, new fantasy films sample

---
+++ nanoGPT/config/train_western_robot_disater.py
@@ -0,0 +1,30 @@
+out_dir = 'out-fantasy'
+eval_interval = 250
+eval_iters = 200
+log_interval = 10
+
+always_save_checkpoint = True
+
+wandb_log = False # override via command line if you like
+wandb_project = 'fantasy'
+wandb_run_name = 'mini-gpt'
+
+init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
+
+dataset = 'fantasy'
+gradient_accumulation_steps = 1
+batch_size = 64
+block_size = 256 # context of up to 256 previous characters
+
+n_layer = 6
+n_head = 6
+n_embd = 384
+dropout = 0.2
+
+learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+max_iters = 10000
+lr_decay_iters = max_iters # make equal to max_iters usually
+min_lr = 1e-4 # learning_rate / 10 usually
+beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+
+warmup_iters = 100
